5d76e76a65dcb0e0d7b9fde4569642ae575be23c
Ivan-Vukusic <ivukusic27@gmail.com>
2023-12-14 20:58:30 +0100

    Unos podataka u tablicu smjer

---
+++ insert.sql
@@ -0,0 +1,10 @@
+use edunovawp3;
+
+select * from smjerovi;
+
+insert into smjerovi (naziv, trajanje, cijena, vaucer)
+values ('Web programiranje', 225, 1859.45, 1);
+
+insert into smjerovi values ('Web dizajn', null, null, null);
+
+insert into smjerovi (naziv) values('čšćđž ŠĐČĆŽ');
