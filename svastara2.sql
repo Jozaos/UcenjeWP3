bc84067161911dc70d15fbcaacdaf4fc12008998
Josip Rebolj <152492711+Jozaos@users.noreply.github.com>
2024-01-18 19:59:26 +0100

    nastavak vježbe

--- svastara2.sql
+++ svastara2.sql
@@ -7,3 +7,21 @@ where redniBroj = '1/2008' order by 1;
 select * from moja;
 
 drop table moja;
+
+
+
+select a.redniBroj, sum(b.cijena*b.kolicina) as ukupno
+from Primke a
+inner join ArtikliNaPrimci b on a.sifra=b.primka
+inner join Artikli c on b.artikl=c.sifra
+where a.datum between '2008-01-01' and '2008-12-31'
+group by a.redniBroj;
+
+select top 10 c.naziv, sum(b.cijena*b.kolicina) as ukupno
+from Primke a
+inner join ArtikliNaPrimci b on a.sifra=b.primka
+inner join dobavljaci c on a.dobavljac=c.sifra
+where a.datum between '2008-01-01' and '2008-12-31'
+group by c.naziv
+having sum(b.cijena*b.kolicina)>1000000
+order by 2 desc;
