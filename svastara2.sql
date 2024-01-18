bd66ae66e7ae3ff595da14ac54ff2b3c379c77ce
Josip Rebolj <152492711+Jozaos@users.noreply.github.com>
2024-01-18 21:08:24 +0100

    još vježbanja

--- svastara2.sql
+++ svastara2.sql
@@ -17,11 +17,26 @@ inner join Artikli c on b.artikl=c.sifra
 where a.datum between '2008-01-01' and '2008-12-31'
 group by a.redniBroj;
 
-select top 10 c.naziv, sum(b.cijena*b.kolicina) as ukupno
+select top 15 percent c.naziv, 
+sum(b.cijena*b.kolicina) as ukupno,
+min(b.cijena*b.kolicina) as minimalno,
+max(b.cijena*b.kolicina) as maksimalno,
+avg(b.cijena*b.kolicina) as prosjecno
 from Primke a
 inner join ArtikliNaPrimci b on a.sifra=b.primka
 inner join dobavljaci c on a.dobavljac=c.sifra
 where a.datum between '2008-01-01' and '2008-12-31'
 group by c.naziv
 having sum(b.cijena*b.kolicina)>1000000
-order by 2 desc;
+order by 5 desc;
+
+
+select datediff(Day,'2022-05-25',getdate())*24*60*70;
+
+select dateadd(Day,-60,getdate());
+
+select power(2,8), sqrt(68), abs(-3);
+
+select top 10 concat(trim(ime),' ',trim(prezime)) as kupac from kupci;
+
+
