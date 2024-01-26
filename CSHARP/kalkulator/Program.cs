fdf498f3068036b37475d4b816346b7d969302a6
Josip Rebolj <152492711+Jozaos@users.noreply.github.com>
2024-01-26 09:40:11 +0100

    Vježbica i izrada kalkulatora

---
+++ CSHARP/kalkulator/Program.cs
@@ -0,0 +1,39 @@
+﻿string value;
+do
+{
+int res;
+Console.Write("Unesi prvi broj u kalkulator: ");
+int Num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Unesi drugi broj u kalkulator: ");
+int Num2 =  Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Unesi željenu operaciju kalkulatora (+,-,/,*): ");
+string Symbol = Console.ReadLine();
+
+switch (Symbol)
+{
+    case "+":
+        res = Num1 + Num2;
+        Console.WriteLine("Zbrajanje: " + res);
+        break;
+    case "-":
+        res = Num1 - Num2;
+        Console.WriteLine("Oduzimanje: " + res);
+        break;
+    case "*":
+        res = Num1 * Num2;
+        Console.WriteLine("Množenje: " + res);
+        break;
+    case "/":
+        res = Num1 / Num2;
+        Console.WriteLine("Dijeljenje: " + res);
+        break;
+    default: Console.WriteLine("Krivi unos!");
+        break;
+}
+Console.WriteLine("Želiš li nastaviti? Da/Ne");
+value = Console.ReadLine();
+   
+} while (value == "Da" || value == "da") ;
+
