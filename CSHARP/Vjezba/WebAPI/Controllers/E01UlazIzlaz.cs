50ecd8c67582c263cf49ba40e41a33c10acd7f3f
Josip Rebolj <152492711+Jozaos@users.noreply.github.com>
2024-01-28 16:23:06 +0100

    Izrada WebAPI za vjezbu

---
+++ CSHARP/Vjezba/WebAPI/Controllers/E01UlazIzlaz.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("Proba")]
+    public class E01UlazIzlaz:ControllerBase
+    {
+        [HttpGet]
+        [Route("v1")]
+        
+        public string Rendom(string Ime)
+        {
+            return "Dobro večer " + Ime;
+        }
+    }
+}
